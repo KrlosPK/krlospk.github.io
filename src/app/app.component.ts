4ca4ccd0dfc49bb5ee53ec914e8775a0aeaae07f
Carlos Morales <carlos.morales@pragma.com.co>
2023-07-04 15:25:15 -0500

    initialized animation library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    Aos.init();
+  }
+}
---
+++ src/app/portfolio/components/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-footer',
+  templateUrl: './footer.component.html',
+  styles: [],
+})
+export class FooterComponent {}
