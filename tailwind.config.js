bc47cc7b8abb51fcf1f90d0b5210defec733ec33
Carlos Morales <carlos.morales@pragma.com.co>
2023-07-04 17:46:55 -0500

    fix: customized app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["ui-sans-serif", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "custom-50": "#111222",
+        "custom-100": "#2C0E37",
+        "custom-200": "#9A879D",
+        "custom-300": "#0C6291",
+        "custom-400": "#C0FDFB",
+        "custom-500": "#F7FFFF",
+      },
+    },
   },
   plugins: [],
 };
