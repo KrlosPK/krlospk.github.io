57ff0f1e8dc807c1d4f106607c06cc84656f088e
Carlos Morales <carlos.morales@pragma.com.co>
2023-07-14 17:41:24 -0500

    fix: change text balance and skills animation

--- src/app/portfolio/components/home/home.component.ts
+++ src/app/portfolio/components/home/home.component.ts
@@ -3,12 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'portfolio-home',
   templateUrl: './home.component.html',
-  styles: [
-    `
-      .text-balance {
-        text-wrap: balance;
-      }
-    `,
-  ],
+  styles: [],
 })
 export class HomeComponent {}
