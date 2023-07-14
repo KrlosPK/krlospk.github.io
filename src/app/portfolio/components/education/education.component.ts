9627d32d39794e0b74cee065a394d8e589bc71d2
Carlos Morales <carlos.morales@pragma.com.co>
2023-07-14 17:40:40 -0500

    feat: change data to json

--- src/app/portfolio/components/education/education.component.ts
+++ src/app/portfolio/components/education/education.component.ts
@@ -2,41 +2,13 @@ import { Component } from '@angular/core';
 
 import { Education } from '../../types/portfolio-types';
 
+import educationList from '../../jsons/educationList.json';
+
 @Component({
   selector: 'portfolio-education',
   templateUrl: './education.component.html',
   styles: [],
 })
 export class EducationComponent {
-  public educationList: Education[] = [
-    {
-      endDate: 'Actualidad',
-      initialDate: 'Febrero 2022',
-      institute: 'SENA',
-      title: 'Tecnólogo en Análisis y Desarrollo de Software',
-    },
-    {
-      endDate: 'Noviembre 2021',
-      href: 'https://krlospk.github.io/assets/documents/Titulo_tecnica_prg.pdf',
-      initialDate: 'Febrero 2020',
-      institute: 'SENA',
-      timeDifference: '1 año y 9 meses',
-      title: 'Técnico en Programación de Software',
-    },
-    {
-      duration: '96 horas',
-      href: 'https://krlospk.github.io/assets/documents/Curso_POO.pdf',
-      initialDate: 'Diciembre 2022',
-      institute: 'SENA',
-      title: 'Curso Desarrollo de Programas Aplicando Metodología POO',
-    },
-    {
-      duration: '77 horas',
-      endDate: 'Junio 2023',
-      href: 'https://krlospk.github.io/assets/documents/OracleNextEducation_Front.pdf',
-      initialDate: 'Mayo 2023',
-      institute: 'Oracle Next Education - Alura Latam',
-      title: 'Curso Desarrollo Frontend',
-    },
-  ];
+  public educationList: Education[] = educationList;
 }
--- src/app/shared/components/buttons/buttons.component.ts
+++ src/app/shared/components/buttons/buttons.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+
 import { type Link } from 'src/app/shared/types/shared-types';
+
+import links from '../../jsons/links.json';
 
 @Component({
   selector: 'shared-buttons',
@@ -7,27 +10,5 @@ import { type Link } from 'src/app/shared/types/shared-types';
   styles: [],
 })
 export class ButtonsComponent {
-  public links: Link[] = [
-    {
-      alt: 'Enlace para mi curriculum',
-      href: 'https://krlospk.github.io/assets/documents/HV_CARLOS_EDUARDO_MORALES_ESTRADA.pdf',
-      img: './assets/logotypes/link.svg',
-      styles: 'bg-custom-200 hover:bg-custom-700',
-      text: 'Curriculum',
-    },
-    {
-      alt: 'Logo de LinkedIn',
-      href: 'https://www.linkedin.com/in/krlos-morales/',
-      img: './assets/logotypes/linkedin.svg',
-      styles: 'bg-custom-300 hover:bg-custom-600',
-      text: 'LinkedIn',
-    },
-    {
-      alt: 'Logo de LinkedIn',
-      href: 'https://github.com/KrlosPK',
-      img: './assets/logotypes/github.svg',
-      styles: 'bg-gray-700 [&>img]:invert hover:bg-gray-800',
-      text: 'GitHub',
-    },
-  ];
+  public links: Link[] = links;
 }
