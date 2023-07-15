720c7364acc8f301b56390d012486ff054a5492c
KrlosPK <centille095@gmail.com>
2023-07-15 15:23:17 -0500

    fix: center arrow image

--- src/app/portfolio/components/home/home.component.ts
+++ src/app/portfolio/components/home/home.component.ts
@@ -3,6 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'portfolio-home',
   templateUrl: './home.component.html',
-  styles: [],
+  styles: [
+    `
+      .portfolio-bounce {
+        animation: portfolio-bounce 1s infinite;
+      }
+
+      @keyframes portfolio-bounce {
+        0%,
+        100% {
+          animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+          transform: translate(-50%, 0);
+        }
+
+        50% {
+          animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+          transform: translate(-50%, 25%);
+        }
+      }
+    `,
+  ],
 })
 export class HomeComponent {}
